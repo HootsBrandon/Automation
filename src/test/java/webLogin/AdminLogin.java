a7ec3fca5279a6a221763ec28a2f26be52712bb2
Brandon Hoots <bhoots@scoir.com>
2018-01-26 16:09:42 -0500

    Login for all users

--- src/test/java/webLogin/AdminLogin.java
+++ src/test/java/webLogin/AdminLogin.java
@@ -1,15 +1,34 @@
 package webLogin;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import pageObjects.LoginPage;
 
 //import pageObjects.LoginPage;
 
 public class AdminLogin {
 	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "/Users/brandonhoots/Webdrivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("https://sif.scoir.com/signin");
+		
+	}
+	
 	@Test
 	public void LoginSuccess() {
 		
-		//LoginPage lp = new LoginPage(null);
+		LoginPage lp = new LoginPage(driver);
+		lp.setUserId("bhoots@scoir.com");
+		lp.setPassword("Test1234");
+		lp.clickSignIn();
+		
 		
 	}
 	
--- src/test/java/webLogin/TeacherLogin.java
+++ src/test/java/webLogin/TeacherLogin.java
@@ -1,11 +1,31 @@
 package webLogin;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import pageObjects.LoginPage;
 
 public class TeacherLogin {
 	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "/Users/brandonhoots/Webdrivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("https://sif.scoir.com/signin");
+		
+	}
+	
 	@Test
 	public void LoginSuccess() {
+		
+		LoginPage lp = new LoginPage(driver);
+		lp.setUserId("bhoots+Greceiver1T7@scoir.com");
+		lp.setPassword("Test1234");
+		lp.clickSignIn();
 		
 	}
 	
