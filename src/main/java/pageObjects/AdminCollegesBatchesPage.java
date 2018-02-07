3ebc3762d2d03d82aef806cedf38c3d265aa06a7
Brandon Hoots <bhoots@scoir.com>
2018-02-07 13:11:56 -0500

    Admin page object class updates

--- src/main/java/pageObjects/AdminCollegesBatchesPage.java
+++ src/main/java/pageObjects/AdminCollegesBatchesPage.java
@@ -1,21 +1,280 @@
 package pageObjects;
-
+ 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class AdminCollegesBatchesPage {
-	
+
 	WebDriver driver;
-	
-	//Loging username
-	By username = By.id("user-email");
-	
-	
-	
-	public AdminCollegesBatchesPage (WebDriver driver) {
-		
+
+	// Documents by high school
+	By docsByHS = By.cssSelector(
+			"body > div.body-wrapper > div.ng-scope > div > div.admin-canvas.ng-scope > div > div > div.ng-scope > div > div > div > div.high-school-filter-wrapper.clearfix > span.typeahead-wrapper > div > div > input");
+
+	// Search
+	By search = By.cssSelector(
+			"body > div.body-wrapper > div.ng-scope > div > div.admin-canvas.ng-scope > div > div > div.ng-scope > div > div > div > div.ng-isolate-scope > div > div.table-actions > ul > div > div.pull-left.mleft-15 > div.search.pull-left.clearfix.ng-scope > form > div > input");
+
+	// Button to determine search results per page
+	By showMe = By.cssSelector(
+			"body > div.body-wrapper > div.ng-scope > div > div.admin-canvas.ng-scope > div > div > div.ng-scope > div > div > div > div.ng-isolate-scope > div > div.table-actions > ul > div > div.pull-left.mleft-15 > div.result-toggle.pull-right.page-size.ng-scope > div > button");
+
+	// value of show me
+	By fifty = By.cssSelector(
+			"body > div.body-wrapper > div.ng-scope > div > div.admin-canvas.ng-scope > div > div > div.ng-scope > div > div > div > div.ng-isolate-scope > div > div.table-actions > ul > div > div.pull-left.mleft-15 > div.result-toggle.pull-right.page-size.ng-scope > div > ul > li:nth-child(2)");
+
+	// Results filter
+	By results = By.cssSelector(
+			"body > div.body-wrapper > div.ng-scope > div > div.admin-canvas.ng-scope > div > div > div.ng-scope > div > div > div > div.ng-isolate-scope > div > div.table-actions > ul > div > div.btn-group.pull-right.mleft-25.col-filter.ng-scope > span.dropdown > button > i");
+
+	// Display college name
+	By college = By.cssSelector(
+			"body > div.body-wrapper > div.ng-scope > div > div.admin-canvas.ng-scope > div > div > div.ng-scope > div > div > div > div.ng-isolate-scope > div > div.table-actions > ul > div > div.btn-group.pull-right.mleft-25.col-filter.ng-scope > span.dropdown.open > ul > li:nth-child(1) > label > span");
+
+	// Display high school
+	By highSchool = By.cssSelector(
+			"body > div.body-wrapper > div.ng-scope > div > div.admin-canvas.ng-scope > div > div > div.ng-scope > div > div > div > div.ng-isolate-scope > div > div.table-actions > ul > div > div.btn-group.pull-right.mleft-25.col-filter.ng-scope > span.dropdown.open > ul > li:nth-child(2) > label > span");
+
+	// Display batch date
+	By batchDate = By.cssSelector(
+			"body > div.body-wrapper > div.ng-scope > div > div.admin-canvas.ng-scope > div > div > div.ng-scope > div > div > div > div.ng-isolate-scope > div > div.table-actions > ul > div > div.btn-group.pull-right.mleft-25.col-filter.ng-scope > span.dropdown.open > ul > li:nth-child(3) > label > span");
+
+	// Display status
+	By status = By.cssSelector(
+			"body > div.body-wrapper > div.ng-scope > div > div.admin-canvas.ng-scope > div > div > div.ng-scope > div > div > div > div.ng-isolate-scope > div > div.table-actions > ul > div > div.btn-group.pull-right.mleft-25.col-filter.ng-scope > span.dropdown.open > ul > li:nth-child(4) > label > span");
+
+	// Force Generate button
+	By forceGenerate = By.cssSelector("body > div.body-wrapper > div.ng-scope > div > div.admin-canvas.ng-scope > div > div > div.ng-scope > div > div > div > div.high-school-filter-wrapper.clearfix > button > scoir-icon > svg");
+
+	// Loging username
+	By forceNo = By.cssSelector("body > div.modal.fade.ng-isolate-scope.in > div > div > div > div.action-buttons-wrapper.clearfix > div > a.btn.btn-link");
+
+	// Loging username
+	By username = By.cssSelector("body > div.modal.fade.ng-isolate-scope.in > div > div > div > div.action-buttons-wrapper.clearfix > div > a.btn.btn-primary");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	// Loging username
+	By username = By.cssSelector("");
+
+	public AdminCollegesBatchesPage(WebDriver driver) {
+
 		this.driver = driver;
-		
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
+	}
+
+	public void click() {
+
+		driver.findElement(admissionsOfficers).click();
+
 	}
 
 }
